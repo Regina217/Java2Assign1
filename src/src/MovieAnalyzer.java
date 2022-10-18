c2ea605910aff80548809e41e5fdc05e9598981b
Regina217 <3066256378>
2022-10-18 23:44:51 +0800

    draft1

--- src/src/MovieAnalyzer.java
+++ src/src/MovieAnalyzer.java
@@ -23,7 +23,8 @@ public class MovieAnalyzer {
     }
 
     public Map<Integer, Integer> getMovieCountByYear() {
-        Map<Integer, Integer> map = movieList.stream().collect(Collectors.groupingBy(Movie::getReleased_Year, Collectors.summingInt(p -> 1)));
+        Map<Integer, Integer> map = movieList.stream().filter((Movie e)-> {return String.valueOf(e.getReleased_Year())!=null;})
+                .collect(Collectors.groupingBy(Movie::getReleased_Year, Collectors.summingInt(p -> 1)));
         List<Map.Entry<Integer, Integer>> list = new ArrayList<Map.Entry<Integer, Integer>>(map.entrySet());
         list.stream().sorted(Comparator.comparing(Map.Entry<Integer, Integer>::getKey).reversed()).collect(Collectors.toList());
         LinkedHashMap<Integer, Integer> target = new LinkedHashMap<>();
@@ -41,7 +42,8 @@ public class MovieAnalyzer {
     }
 
     public Map<String, Integer> getMovieCountByGenre() {
-        LinkedHashMap<String, Integer> map = movieList.stream().collect(Collectors.groupingBy(Movie::getGenre, LinkedHashMap::new, Collectors.summingInt(p -> 1)));
+        LinkedHashMap<String, Integer> map = movieList.stream().filter((Movie e)-> {return e.getGenre()!=null;})
+                .collect(Collectors.groupingBy(Movie::getGenre, LinkedHashMap::new, Collectors.summingInt(p -> 1)));
         List<Map.Entry<String, Integer>> list = new ArrayList<Map.Entry<String, Integer>>(map.entrySet());
         list.stream().sorted(Comparator.comparing(Map.Entry<String, Integer>::getValue).reversed().thenComparing(e -> e.getKey().charAt(0))).collect(Collectors.toList());
         LinkedHashMap<String, Integer> target = new LinkedHashMap<>();
@@ -51,55 +53,64 @@ public class MovieAnalyzer {
         return target;
     }
 
-    public Map<List<String>, Integer> getCoStarCount(){
-        List<List<String>> coStarList=new ArrayList<>();
-        for (Movie e:movieList) {
-            for (List<String> f:e.getCostarList()) {
+    public Map<List<String>, Integer> getCoStarCount() {
+        List<List<String>> coStarList = new ArrayList<>();
+        for (Movie e : movieList) {
+            for (List<String> f : e.getCostarList()) {
                 coStarList.add(f);
             }
         }
-        Map<List<String>,Integer>  target=coStarList.stream().collect(Collectors.groupingBy(e->e,Collectors.summingInt(p->1)));
+        Map<List<String>, Integer> target = coStarList.stream().collect(Collectors.groupingBy(e -> e, Collectors.summingInt(p -> 1)));
         return target;
     }
 
-   public List<String> getTopMovies(int top_k, String by){
-        List<String> target=new ArrayList<>();
-        if (by.equals("runtime")){
-            movieList.stream().sorted(Comparator.comparing(Movie::getRuntime).reversed().thenComparing(e -> e.getSeries_Title().charAt(0))).collect(Collectors.toList());
+    public List<String> getTopMovies(int top_k, String by) {
+        List<String> target = new ArrayList<>();
+        if (by.equals("runtime")) {
+            movieList.stream().filter((Movie e)-> {return String.valueOf(e.getRuntime())!=null;})
+                    .sorted(Comparator.comparing(Movie::getRuntime).reversed().thenComparing(e -> e.getSeries_Title().charAt(0))).collect(Collectors.toList());
             for (int i = 0; i < top_k; i++) {
                 target.add(movieList.get(i).getSeries_Title());
             }
-        return target;}
-        else if (by.equals("overview")){
-            movieList.stream().sorted(Comparator.comparing((Movie e)->{return e.getOverview().length();}).reversed().thenComparing(e -> e.getSeries_Title().charAt(0))).collect(Collectors.toList());
+            return target;
+        } else if (by.equals("overview")) {
+            movieList.stream().filter((Movie e)-> {return e.getOverview()!=null;})
+                    .sorted(Comparator.comparing((Movie e) -> {return e.getOverview().length();})
+                            .reversed().thenComparing(e -> e.getSeries_Title().charAt(0))).collect(Collectors.toList());
             for (int i = 0; i < top_k; i++) {
                 target.add(movieList.get(i).getSeries_Title());
             }
-        return target;}
-        else return null;
-   }
+            return target;
+        } else return null;
+    }
 
-    public List<String> getTopStars(int top_k, String by){
-        List<String> target=new ArrayList<>();
-        Map<String,Float> r=new HashMap<>();
-        Map<String,Long> g=new HashMap<>();
-        for (Movie m:movieList) {
-            for (String star:m.getStarList()) {
-                r.put(star,m.getIMDB_Rating());
-                g.put(star,m.getGross());
+    public List<String> getTopStars(int top_k, String by) {
+        List<String> target = new ArrayList<>();
+        Map<String, Float> r = new HashMap<>();
+        Map<String, Long> g = new HashMap<>();
+        for (Movie m : movieList) {
+            for (String star : m.getStarList()) {
+                if(star!=null&&String.valueOf(m.getIMDB_Rating())!=null){r.put(star, m.getIMDB_Rating());}
+                if(star!=null&&String.valueOf(m.getGross())!=null){g.put(star, m.getGross());}
             }
         }
-        if(by.equals("rating")){
-            List<Map.Entry<String,Double>> temp=new ArrayList<>(r.entrySet().stream().collect(Collectors.groupingBy(Map.Entry::getKey,Collectors.averagingDouble(Map.Entry::getValue))).entrySet());
-            temp.stream().sorted(Map.Entry.<String, Double>comparingByValue().reversed().thenComparing((Map.Entry<String,Double> e)->{return e.getKey().charAt(0);}))
+        if (by.equals("rating")) {
+            List<Map.Entry<String, Double>> temp = new ArrayList<>(r.entrySet().stream().collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.averagingDouble(Map.Entry::getValue))).entrySet());
+            temp.stream().sorted(Map.Entry.<String, Double>comparingByValue().reversed().thenComparing((Map.Entry<String, Double> e) -> {
+                        return e.getKey().charAt(0);
+                    }))
                     .collect(Collectors.toList());
             for (int i = 0; i < top_k; i++) {
                 target.add(temp.get(i).getKey());
             }
         }
-        if(by.equals("gross")){
-            List<Map.Entry<String,Double>> temp=new ArrayList<>(g.entrySet().stream().collect(Collectors.groupingBy(Map.Entry::getKey,Collectors.averagingDouble(Map.Entry::getValue))).entrySet());
-            temp.stream().sorted(Comparator.comparing((Map.Entry<String,Double> e)->{return e.getValue();}).reversed().thenComparing((Map.Entry<String,Double> e)->{return e.getKey().charAt(0);}));
+        if (by.equals("gross")) {
+            List<Map.Entry<String, Double>> temp = new ArrayList<>(g.entrySet().stream().collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.averagingDouble(Map.Entry::getValue))).entrySet());
+            temp.stream().sorted(Comparator.comparing((Map.Entry<String, Double> e) -> {
+                return e.getValue();
+            }).reversed().thenComparing((Map.Entry<String, Double> e) -> {
+                return e.getKey().charAt(0);
+            }));
             for (int i = 0; i < top_k; i++) {
                 target.add(temp.get(i).getKey());
             }
@@ -107,11 +118,12 @@ public class MovieAnalyzer {
         return target;
     }
 
-    public List<String> searchMovies(String genre, float min_rating, int max_runtime){
-        List<String> target= new ArrayList<>();
-        movieList.stream().filter(e->e.getIMDB_Rating()>=min_rating&&e.getRuntime()<=max_runtime&&e.getGenre().equals(genre))
-                .sorted(Comparator.comparing(e->e.getGenre().charAt(0))).collect(Collectors.toList());
-        for (Movie m:movieList) {
+    public List<String> searchMovies(String genre, float min_rating, int max_runtime) {
+        List<String> target = new ArrayList<>();
+        movieList.stream().filter((Movie e)-> {return e.getGenre()!=null&&String.valueOf(e.getRuntime())!=null&&String.valueOf(e.getIMDB_Rating())!=null;})
+                .filter(e -> e.getIMDB_Rating() >= min_rating && e.getRuntime() <= max_runtime && e.getGenre().equals(genre))
+                .sorted(Comparator.comparing(e -> e.getGenre().charAt(0))).collect(Collectors.toList());
+        for (Movie m : movieList) {
             target.add(m.getSeries_Title());
         }
         return target;
@@ -174,8 +186,8 @@ public class MovieAnalyzer {
             return Star4;
         }
 
-        public List<String> getStarList(){
-            List<String> target=new ArrayList<>();
+        public List<String> getStarList() {
+            List<String> target = new ArrayList<>();
             target.add(getStar1());
             target.add(getStar2());
             target.add(getStar3());
@@ -189,12 +201,14 @@ public class MovieAnalyzer {
             for (int i = 0; i < starList.size(); i++) {
                 for (int j = i + 1; j < starList.size(); j++) {
                     List<String> targetPart = new ArrayList<>();
-                    if (starList.get(j).charAt(0) - starList.get(i).charAt(0) > 0) {
-                        targetPart.add(starList.get(i));
-                        targetPart.add(starList.get(j));
-                    } else targetPart.add(starList.get(j));
+                    if (starList.get(i) != null && starList.get(j) != null){
+                        if (starList.get(j).charAt(0) - starList.get(i).charAt(0) > 0) {
+                            targetPart.add(starList.get(i));
+                            targetPart.add(starList.get(j));
+                        } else targetPart.add(starList.get(j));
                     targetPart.add(starList.get(i));
                     target.add(targetPart);
+                }
                 }
             }
             return target;
@@ -220,7 +234,7 @@ public class MovieAnalyzer {
             return Gross;
         }
     }
-
+}
 //    public static boolean isInTheMovie(String star, Movie movie) {
 //        if (movie.getStar1().equals(star)) {
 //            return true;
@@ -238,4 +252,3 @@ public class MovieAnalyzer {
 //            return true;
 //        } else return false;
 //    }
-}
